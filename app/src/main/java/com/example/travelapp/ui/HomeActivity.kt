6d1b09e356edaeae5d2dc31e1aaffd35bb520ee5
slax <slax.xals@gmail.com>
2023-03-23 21:30:43 +0200

    feat: Use view binding

--- app/src/main/java/com/example/travelapp/ui/HomeActivity.kt
+++ app/src/main/java/com/example/travelapp/ui/HomeActivity.kt
@@ -3,30 +3,36 @@ package com.example.travelapp.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.ImageView
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.example.travelapp.R
+import com.example.travelapp.databinding.ActivityHomeBinding
 import com.example.travelapp.ui.adventure.AdventureActivity
 import com.example.travelapp.ui.tickets.TicketsActivity
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
-        findViewById<ConstraintLayout>(R.id.tickets).setOnClickListener{
-            this.startActivity(
-                Intent(this, TicketsActivity::class.java)
-            )
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initOnClickListeners()
+    }
+
+    private fun initOnClickListeners() {
+        binding.tickets.setOnClickListener {
+            openActivity(TicketsActivity::class.java)
         }
-        findViewById<ConstraintLayout>(R.id.adventure).setOnClickListener{
-            this.startActivity(
-                Intent(this, AdventureActivity::class.java)
-            )
+        binding.adventure.setOnClickListener{
+            openActivity(AdventureActivity::class.java)
         }
     }
+
+    private fun openActivity(cls: Class<*>) {
+        this.startActivity(
+            Intent(this, cls)
+        )
+    }
+
 }
--- app/src/main/java/com/example/travelapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/travelapp/ui/MainActivity.kt
@@ -3,48 +3,25 @@ package com.example.travelapp.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import com.example.travelapp.R
+import com.example.travelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.i("Lifecycle", "onCreate")
 
-        val intent = Intent(this, HomeActivity::class.java)
-        val skipText = findViewById<TextView>(R.id.skipText)
-        skipText.setOnClickListener { startActivity(intent) }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initOnClickListeners()
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.i("Lifecycle", "onStart")
+    private fun initOnClickListeners() {
+        binding.skipText.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.i("Lifecycle", "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("Lifecycle", "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("Lifecycle", "onStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.i("Lifecycle", "onRestart")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("Lifecycle", "onDestroy")
-    }
 }
