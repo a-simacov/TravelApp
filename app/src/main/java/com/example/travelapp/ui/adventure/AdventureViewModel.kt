86f5b14ea8d1178a8fcec3c45ae71612ec7eaf4a
slax <slax.xals@gmail.com>
2023-04-04 18:37:26 +0300

    feat: Add lesson 17 task 01

--- app/src/main/java/com/example/travelapp/ui/adventure/AdventureViewModel.kt
+++ app/src/main/java/com/example/travelapp/ui/adventure/AdventureViewModel.kt
@@ -1,15 +1,18 @@
 package com.example.travelapp.ui.adventure
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.travelapp.db.Db
 import com.example.travelapp.db.Places
 import com.example.travelapp.db.Repository
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlin.system.measureTimeMillis
 
 class AdventureViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -36,9 +39,23 @@ class AdventureViewModel(application: Application) : AndroidViewModel(applicatio
     }
 
     fun add(place: Places) {
-        CoroutineScope(Dispatchers.Unconfined).launch {
-            repository.addPlace(place)
+        val map = mutableMapOf<CoroutineDispatcher, Long>(
+            Dispatchers.Main to 0,
+            Dispatchers.Unconfined to 0,
+            Dispatchers.IO to 0,
+            Dispatchers.Default to 0
+        )
+        map.forEach { (disp, _) ->
+            map[disp] = measureTimeMillis {
+                CoroutineScope(disp).launch {
+                    repository.addPlace(place)
+                }
+            }
+            Log.i("DispatchersTime", "$disp time: ${map[disp]}")
         }
+
+        val winner = map.minBy {it.value}
+        Log.i( "DispatchersTime", "Winner is $winner")
     }
 
 }
--- app/src/main/java/com/example/travelapp/ui/tickets/TicketsViewModel.kt
+++ app/src/main/java/com/example/travelapp/ui/tickets/TicketsViewModel.kt
@@ -1,13 +1,16 @@
 package com.example.travelapp.ui.tickets
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.travelapp.db.*
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlin.system.measureTimeMillis
 
 // используется именно AndroidViewModel, т.к. она может принимать контекст
 class TicketsViewModel(application: Application) : AndroidViewModel(application) {
@@ -35,9 +38,23 @@ class TicketsViewModel(application: Application) : AndroidViewModel(application)
     }
 
     fun add(ticket: Ticket) {
-        CoroutineScope(Dispatchers.Unconfined).launch {
-        repository.addTicket(ticket)
+        val map = mutableMapOf<CoroutineDispatcher, Long>(
+            Dispatchers.Main to 0,
+            Dispatchers.Unconfined to 0,
+            Dispatchers.IO to 0,
+            Dispatchers.Default to 0
+        )
+        map.forEach { (disp, _) ->
+            map[disp] = measureTimeMillis {
+                CoroutineScope(disp).launch {
+                    repository.addTicket(ticket)
+                }
+            }
+            Log.i("DispatchersTime", "$disp time: ${map[disp]}")
         }
+
+        val winner = map.minBy {it.value}
+        Log.i( "DispatchersTime", "Winner is $winner")
     }
 
     fun deleteById(id: Int) {
